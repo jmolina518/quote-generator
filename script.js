f3c8483842cf3496dc087501041b688c8e75157f
jmolina518 <jmolina518@gmail.com>
2021-03-05 21:21:38 -0600

    add twitterBtn const

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const quoteContainer = document.getElementById('quote-container')
 const quoteText = document.getElementById('quote')
 const authorText = document.getElementById('author')
+const twitterBtn = document.getElementById('twitter')
 
 let apiQuotes = []
 
